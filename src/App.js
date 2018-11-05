4f877f8d489b627b7f5dc126f08bcee889850a62
patti-c <patricia.carter@flatironschool.com>
2018-11-05 10:49:35 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import './App.css'
-import Form from './Form'
-import Order from './Order'
+import Form from './components/Form'
+import Order from './components/Order'
 
 class App extends Component {
-  state: {
-    orders: []
+
+  constructor() {
+    super()
+    this.state = {
+      orders: []
+    }
   }
 
   addOrder = (order) => {
@@ -15,9 +19,14 @@ class App extends Component {
   }
 
   render() {
-    const orders = this.state.orders.map( (order, idx) => {
-      <Order key={idx} {...order} />
-    })
+
+    let orders = null
+
+    if(this.state.orders) {
+      orders = this.state.orders.map( (order, idx) => {
+        return <Order key={idx} {...order} />
+      })
+    }
 
     return (
       <div className="App">
@@ -25,7 +34,7 @@ class App extends Component {
           <img src={ require('./images/logo.png') } className="App-logo" alt="logo" />
         </header>
 
-        <Form />
+        <Form addOrder={this.addOrder}/>
 
         <div className="ui raised container segment">
           <h1 className="ui block header">All Orders</h1>
